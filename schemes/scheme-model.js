586e9975cb264855b623caef52055f1930908c6d
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-11-06 10:15:19 -0600

    Implemented all get requests for each scheme, scheme by ID, and all steps associated with schemes, through funcions: find, findById, findSteps.

---
+++ schemes/scheme-model.js
@@ -0,0 +1,24 @@
+const db = require("../data/db-config");
+
+module.exports = {
+    find,
+    findById,
+    findSteps
+}
+
+function find() {
+    return db('schemes');
+}
+
+function findById(id){
+    return db('schemes')
+        .where({ id })
+        .first();
+}
+
+function findSteps(id){
+    return db('schemes as s')
+        .join("steps as st", "s.id", "st.scheme_id")
+        .select("st.id", "s.scheme_name", "st.step_number", "st.instructions")
+        .where({ scheme_id: id })
+}
