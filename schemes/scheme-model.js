5b373eb55e73ef17e5dc98d8a144e81b07377cda
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-11-06 10:19:21 -0600

    Added add() functionality for post requests.

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,7 +3,8 @@ const db = require("../data/db-config");
 module.exports = {
     find,
     findById,
-    findSteps
+    findSteps,
+    add
 }
 
 function find() {
@@ -20,5 +21,11 @@ function findSteps(id){
     return db('schemes as s')
         .join("steps as st", "s.id", "st.scheme_id")
         .select("st.id", "s.scheme_name", "st.step_number", "st.instructions")
-        .where({ scheme_id: id })
+        .where({ scheme_id: id });
+}
+
+function add(updatedScheme){
+    return db('schemes')
+        .insert(updatedScheme)
+        .then(([id]) => findById(id))
 }
